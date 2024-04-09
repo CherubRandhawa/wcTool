953fd14d9e1a496f9c0538abf93f767f3cfc3a9a
cherub randhawa <cherubs1897@gmail.com>
2024-04-09 16:13:40 -0400

    docstrings and method description

--- ccwc.py
+++ ccwc.py
@@ -2,9 +2,27 @@ import sys
 import os 
 
 def count_bytes(file_path):
+    '''
+    Counts the file size
+
+    Parameters:
+    - file_path: path to the file to be scanned
+
+    Output:
+    - returns the size of the file or either raises error if file is not found
+    '''
     return os.path.getsize(file_path)
 
 def count_lines(file_path):
+    '''
+    Counts the number of lines in specified file
+
+    Parameters:
+    - file_path: path to the file to be scanned
+
+    Output:
+    - returns the number of lines in a text file
+    '''
     lines = 0
     try:
         with open(file_path, 'r', encoding='utf-8') as file:

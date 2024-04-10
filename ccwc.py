c74d83e3742b2a40b1975cf3e9ce4f6108fecd5c
cherub randhawa <cherubs1897@gmail.com>
2024-04-09 22:00:46 -0400

    Added char count and word count functionality with method docstrings

--- ccwc.py
+++ ccwc.py
@@ -21,7 +21,7 @@ def count_lines(file_path):
     - file_path: path to the file to be scanned
 
     Output:
-    - returns the number of lines in a text file
+    - returns the line count in a text file
     '''
     lines = 0
     try:
@@ -32,6 +32,47 @@ def count_lines(file_path):
     except FileNotFoundError:
         print('File not found.')
         return None
+
+def count_words(file_path):
+    '''
+    Counts the number of words in a text file
+
+    Parameters: 
+    -file_path: path of the file to be scanned
+
+    Output:
+    - returns word count in a text file 
+    '''
+    words = 0
+    try:
+        with open(file_path, 'r', encoding = 'utf-8') as file:
+            for line in file:
+                words += len(line.split())
+            return words
+    except FileNotFoundError:
+        print('File not found')
+        return None
+    
+def count_chars(file_path):
+    '''
+    Counts the number of characters in a character file
+
+    Parameters:
+    -file_path: path of the file to be scanned
+
+    Output:
+    - returns the character count of a text file
+    '''
+    characters = 0
+    try:
+        with open(file_path, 'r', encoding = 'utf-8') as file:
+            for line in file:
+                characters += len(line)
+            return characters
+    except FileNotFoundError:
+        print("File not found")
+        return None
+
 
 def main():
     if  len(sys.argv) != 3:
@@ -44,8 +85,15 @@ def main():
         bytes_count = count_bytes(file_path)
         print(f'{bytes_count} {file_path}')
     elif option == '-l':
-        lines_count = count_lines(file_path)
-        print(f'{lines_count} {file_path}')
+        line_count = count_lines(file_path)
+        print(f'{line_count} {file_path}')
+    elif option == '-w':
+        word_count = count_words(file_path)
+        print(f'{word_count} {file_path}')
+    elif option == '-m':
+        char_count = count_chars(file_path)
+        print(f'{char_count} {file_path}')
+
 
 if __name__ == '__main__':
     main()
